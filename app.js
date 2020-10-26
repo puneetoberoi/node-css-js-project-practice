73697fc07e7ad21abc9d750a3eb4cc869e6c58c6
puneetoberoi <59065458+puneetoberoi@users.noreply.github.com>
2020-10-26 12:00:54 -0700

    adding views

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ const app = express();
 //port for the server and environment variable
 const port = process.env.PORT || 3000
 
+//set viewing engines
+app.set('view engine', 'pug')
+app.set('views', './views')//default
+
 app.get('/', (req, res)=>{
-    res.send('Hello World')
+    res.render('index', {title: "Nanak", message: "Waheguru"})
 })
 
 app.listen(port, ()=>{
